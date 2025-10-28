de20124eeee03ade698b83793a076e4b397fd0af
sitra <sitradaru@gmail.com>
2025-10-28 01:31:24 -0700

    Add getHumanChoice function to get player input

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function getComputerChoice() {
     let shoot=Math.floor(Math.random()*3);
     return choice[shoot];
 }
+
+function getHumanChoice() {
+    let shoot=prompt("Shoot your shot/rock, paper or scissor");
+    return shoot;
+}
